ebf3a15a4bd4116409e01a3cc449ecda3d4f5b2d
Renato Almeida <renatoam@ymail.com>
2020-08-11 23:51:48 -0300

    feat: added User model

--- backend/src/database/migrations/1597200044750-CreateUsers.ts
+++ backend/src/database/migrations/1597200044750-CreateUsers.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1597110884575 implements MigrationInterface {
+export default class CreateUsers1597200044750 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [{
           name: 'id',
           type: 'varchar',
@@ -13,13 +13,19 @@ export default class CreateAppointments1597110884575 implements MigrationInterfa
           generationStrategy: "uuid",
           default: 'uuid_generate_v4()'
         }, {
-          name: "provider",
+          name: "name",
           type: "varchar",
           isNullable: false
         }, {
-          name: "date",
-          type: "timestamp with time zone",
-          isNullable: false
+          name: "email",
+          type: "varchar",
+          isNullable: false,
+          isUnique: true
+        },
+        {
+          name: "password",
+          type: "varchar",
+          isNullable: false,
         },
         {
           name: "created_at",
@@ -36,7 +42,7 @@ export default class CreateAppointments1597110884575 implements MigrationInterfa
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments')
+    await queryRunner.dropTable('users')
   }
 
 }
