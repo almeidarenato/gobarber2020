3808848fb1cf8658bdbe04d0c0097a26f8f46538
Renato Almeida <renatoam@ymail.com>
2020-08-13 19:00:28 -0300

    feat: added img upload route

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import CreateUserService from '../service/CreateUserService'
-const usersRouter = Router();
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
+
+import CreateUserService from '../service/CreateUserService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+
+const usersRouter = Router();
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -16,6 +22,10 @@ usersRouter.post('/', async (request, response) => {
   catch (err) {
     return response.status(400).json({ error: err.message })
   }
+})
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  return response.json({ ok: true })
 })
 
 export default usersRouter
