37fccbbed1d9f2b0353055574646b12f6403a260
Renato Almeida <renatoam@ymail.com>
2020-08-13 23:36:50 -0300

    feat: added global error handler middleware

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -26,18 +26,15 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  try {
-    const updateUserAvatar = new UpdateUserAvatarService();
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename
-    })
-    delete user.password
-    return response.json(user)
-  }
-  catch (err) {
-    return response.status(400).json({ error: err.message })
-  }
+
+  const updateUserAvatar = new UpdateUserAvatarService();
+  const user = await updateUserAvatar.execute({
+    user_id: request.user.id,
+    avatarFilename: request.file.filename
+  })
+  delete user.password
+  return response.json(user)
+
 })
 
 export default usersRouter
